3197d943c7aebae167458c4e6033798b9ae15868
Opeyemi <toheebopeyemi9@gmail.com>
2025-03-09 15:44:57 +0100

    id section

--- src/components/IdSection.tsx
+++ src/components/IdSection.tsx
@@ -1,5 +1,8 @@
 export default function IdSection({ id }: { id: string }) {
   return (
-    <div id={id} className="absolute top-0 left-0 -translate-y-1/2 p-24" />
+    <div
+      id={id}
+      className="absolute top-0 left-0 z-[-5] -translate-y-1/2 p-24"
+    />
   );
 }
